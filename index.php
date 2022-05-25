d9ecd5716e39917a0241acde768a0bbdca5876e4
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-25 17:16:38 +0200

    added cards

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-include __DIR__ . '/database.php'
+include __DIR__ .'/database.php';
+
 
 ?>
 
@@ -14,20 +15,29 @@ include __DIR__ . '/database.php'
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
-    <title>Dischi</title>
+    <title>php Dischi</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 justif">
-                <h1>ciao1</h1>
+    <header>
+        <nav>
+            <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="logo">
+        </nav>
+    </header>
+    <main>
+        <div class="container  ">
+            <div class="row ">
+                <?php foreach($dischi as $disco){ ?>
+                <div class="card-container text-center ">
+                    <img src="<?php echo $disco['poster']  ?>" alt="disco.title">
+                    <h5> <?php echo $disco['title'] ?></h5>
+                    <p><?php echo $disco['author'] ?></p>
+                    <p><?php echo $disco['year'] ?></p>
+                </div>
+                <?php } ?>
             </div>
-            
         </div>
-    </div>
-    
-
+    </main>
 </body>
 
 </html>
