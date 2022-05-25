e015155055af7a042517343372edd56efc790897
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-25 16:15:59 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+include __DIR__ . '/database.php'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Dischi</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 justif">
+                <h1>ciao1</h1>
+            </div>
+            
+        </div>
+    </div>
+    
+
+</body>
+
+</html>
